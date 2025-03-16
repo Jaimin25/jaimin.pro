b96e81f57d329e15ee788b8e040bb7d8ebe12d59
Jaimin Chovatia <chovatiajaimin@gmail.com>
2025-03-16 23:19:12 +0530

    feat: add @next/third-parties dependency and integrate Google Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { RESUME } from "@/lib/resume";
 import Header from "@/components/Layout/header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: `Hey! It's me ${RESUME.initials}`,
@@ -23,6 +24,7 @@ export default function RootLayout({
         />
       </head> */}
       <body className={`font-poppins antialiased grid justify-center`}>
+        <GoogleAnalytics gaId={process.env.GOOGLE_TAG!} />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           {children}
